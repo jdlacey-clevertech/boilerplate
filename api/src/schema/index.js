07d5aa436f62bd06a234a3a5fad9a097084affa9
Chris Buryta <chris@buryta.com>
2019-02-24 16:27:58 -0500

    add data sources and example extra schema to apollo api

--- api/src/schema/index.js
+++ api/src/schema/index.js
@@ -1,12 +1,45 @@
-const { typeDefs: SampleTypeDefs, resolvers: SampleResolvers } = require('./Sample')
+// pull in and build the type definitions and resolvers in this file
+// other files in `/schema` can be limited to app objects/contexts
 
-const typeDefs = [SampleTypeDefs]
+const {
+  typeDefs: GlobalTypeDefs,
+  resolvers: GlobalResolvers
+} = require('./Global')
 
+const {
+  typeDefs: DateTimeTypeDefs,
+  resolvers: DateTimeResolvers
+} = require('./DateTime')
+
+const {
+  typeDefs: SampleTypeDefs,
+  resolvers: SampleResolvers
+} = require('./Sample')
+
+// {
+//   typeDefs: AuthTypeDefs,
+//   resolvers: AuthResolvers
+// } = require('./Auth')
+
+// merge resolvers
 const resolvers = {
+  ...GlobalResolvers,
+  ...DateTimeResolvers,
+  // ...AuthResolvers,
   ...SampleResolvers
 }
 
+// merge graphql type definitions
+const typeDefs = [
+  GlobalTypeDefs,
+  DateTimeTypeDefs,
+  // AuthTypeDefs,
+  SampleTypeDefs
+]
+
+// export utils from the schema for use by the ApolloServer config
 module.exports = {
   typeDefs,
   resolvers
+  // ,CognitoAuthDirective
 }
